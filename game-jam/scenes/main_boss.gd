1f19b750dab06df36a70867d1d4d1963460462d3
sajjad-alkhummais <58751460+sajjad-alkhummais@users.noreply.github.com>
2025-11-16 10:25:35 +0000

    Following the player code

--- game-jam/scenes/main_boss.gd
+++ game-jam/scenes/main_boss.gd
@@ -1,8 +1,44 @@
 extends CharacterBody2D
 
-@onready var animation_player = $AnimationPlayer
+var mainBossPositions = [Vector2(430,230), Vector2(630,230)]
 
+var direction = null;
 var playerInRange = false;
+@export var MOVE_SPEED: float = 100.0
+@onready var animation_player = $AnimationPlayer
+@onready var animated_sprite = $AnimatedSprite2D
+# This variable will store a reference to the player (the "cat")
+var player_target: Node2D = null
+# The _ready() function runs once when the boss enters the scene.
+# We'll use it to find the player.
+func _ready():
+	# This line searches the entire game scene
+	# for the first node in the "player" group.
+	player_target = get_tree().get_first_node_in_group("player")
+	# This is a safety check in case you forget to add the player to the group.
+	if player_target == null:
+		print("MAIN BOSS ERROR: Could not find any node in the 'player' group. The boss will not move.")
+	else:
+		direction = (player_target.global_position - global_position).normalized()
+	
+func _physics_process(delta):
+	
+	if not player_target:
+		return
+
+	direction = (player_target.global_position - global_position).normalized()
+	if not playerInRange:
+		move_and_slide()
+	else:
+		velocity = Vector2.ZERO
+		move_and_slide()
+	
+	if velocity.x > 0:
+		animated_sprite.flip_h = false
+	elif velocity.x < 0:
+		animated_sprite.flip_h = true
+
+
 var attacks = ["Attack1", "Attack2"]
 func on_animation_finished(anim_name: StringName) -> void:
 	if(playerInRange):
@@ -10,9 +46,7 @@ func on_animation_finished(anim_name: StringName) -> void:
 		animation_player.play(attackType)
 	else:
 		animation_player.play("idle")
-	
 	print("test, idle")
-
 func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
 		playerInRange = true
@@ -27,4 +61,12 @@ func _on_hitbox_entered(body: Node2D) -> void:
 
 
 func _on_detection_body_exited(body: Node2D) -> void:
+	
 	playerInRange = false;
+
+
+func _on_timer_timeout() -> void:
+	print("3 sec passed")
+		# First, check if we successfully found the player in the _ready() function.
+	if not playerInRange:
+		velocity = direction * MOVE_SPEED
